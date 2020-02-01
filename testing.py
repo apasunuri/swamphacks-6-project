b4ca1b625ec88bcea982f4a4976245bb71faf825
Navid <navidali@ufl.edu>
2020-02-01 02:13:10 -0500

    register route

--- testing.py
+++ testing.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/registration")
+def register():
+    return render_template("register.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
